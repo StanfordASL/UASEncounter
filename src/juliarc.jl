18d08ce6d574bd59654b0a4bf3d364ba9233546f
Zachary Sunberg <sunbergzach(at)gmail.com>
2015-04-16 13:51:44 -0700

    added juliarc

---
+++ src/juliarc.jl
@@ -0,0 +1,8 @@
+println("executing local juliarc")
+
+@everywhere mdp_path = "/home/zach/.julia/mdp/"
+@everywhere include("$(mdp_path)src/MDP.jl")
+@everywhere include("$(mdp_path)src/mdps/GridWorld.jl")
+@everywhere include("$(mdp_path)src/other/auxfuncs.jl")
+# include("$(mdp_path)src/solvers/MCTSdpw/AG.jl")
+@everywhere include("$(mdp_path)src/solvers/MCTSdpw/MCTSdpw.jl")
