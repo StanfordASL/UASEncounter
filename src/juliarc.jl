4174510fa54f8cb07a2d31fb2f11783ae2f5fa01
Zachary Sunberg <sunbergzach(at)gmail.com>
2015-04-16 16:11:28 -0700

    used Pkg.dir for mdp path

--- src/juliarc.jl
+++ src/juliarc.jl
@@ -1,6 +1,6 @@
 println("executing local juliarc")
 
-@everywhere mdp_path = "$(homedir())/.julia/mdp/"
+@everywhere mdp_path = "$(Pkg.dir())/mdp/"
 @everywhere include("$(mdp_path)src/MDP.jl")
 @everywhere include("$(mdp_path)src/mdps/GridWorld.jl")
 @everywhere include("$(mdp_path)src/other/auxfuncs.jl")
